842c4629ba2593920dc1a43c7a5040481f51da20
Gravitano <gravitano16@gmail.com>
2015-03-22 18:31:26 +0700

    replace home route with post route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+Route::get('post/{slug}', 'PostsController@show');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
