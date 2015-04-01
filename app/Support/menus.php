2b6255a5684bce46d4dac8faf445a49aaac56a36
Gravitano <gravitano16@gmail.com>
2015-04-01 23:45:24 +0700

    Add 'active' & 'inactive' state

--- app/Support/menus.php
+++ app/Support/menus.php
@@ -5,14 +5,38 @@ Menu::create('admin.left', function ($menu)
 	$menu->route('admin.index', 'Home');
 	$menu->dropdown('Posts', function ($sub)
 	{
-		$sub->route('admin.posts.index', 'All Posts');
-		$sub->route('admin.posts.create', 'Add New');
-	});
+		$sub->route('admin.posts.index', 'All Posts', [], [
+			'inactive' => function ()
+			{
+				return URL::full() == route('admin.posts.index', ['type' => 'page']);
+			}
+		]);
+		$sub->route('admin.posts.create', 'Add New', [], ['inactive' => function ()
+			{
+				return URL::full() == route('admin.posts.create', ['type' => 'page']);
+			}]);
+	}, [
+			'inactive' => function ()
+			{
+				return URL::full() == route('admin.posts.index', ['type' => 'page']);
+			}
+		]);
 	$menu->dropdown('Pages', function ($sub)
 	{
-		$sub->route('admin.posts.index', 'All Pages', ['type' => 'page']);
-		$sub->route('admin.posts.create', 'Add New', ['type' => 'page']);
-	});
+		$sub->route('admin.posts.index', 'All Pages', ['type' => 'page'], [
+			'active' => function ()
+			{
+				return URL::full() == route('admin.posts.index', ['type' => 'page']);
+			}
+		]);
+		$sub->route('admin.posts.create', 'Add New', ['type' => 'page'], ['active' => function ()
+			{
+				return URL::full() == route('admin.posts.create', ['type' => 'page']);
+			}]);
+	}, ['active' => function () {
+		return URL::full() == route('admin.posts.index', ['type' => 'page']) || 
+		URL::full() == route('admin.posts.create', ['type' => 'page']) ;
+	}]);
 	$menu->dropdown('Categories', function ($sub)
 	{
 		$sub->route('admin.categories.index', 'All Categories');
