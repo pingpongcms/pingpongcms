658a479f6199af09e2f68102b174e66891a2ed55
Ahmad Karlam <ahmad.karlam@gmail.com>
2015-04-01 20:07:35 +0700

    Add category request

---
+++ app/Http/Requests/Admin/Categories/UpdateCategoryRequest.php
@@ -0,0 +1,27 @@
+<?php namespace App\Http\Requests\Admin\Categories;
+
+use App\Http\Requests\Request;
+
+class UpdateCategoryRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [];
+	}
+
+}
