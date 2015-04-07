02bc515b7fd87174a8730a7a3ce1a4a8bd342c26
Gravitano <gravitano16@gmail.com>
2015-04-07 12:07:10 +0700

    Add icons & update backend title

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Laravel</title>
+		<title>@yield('title', 'Administrator')</title>
 		@yield('head')
 		<!-- Bootstrap CSS -->
 		<link href="{!! asset('components/bootstrap/dist/css/bootstrap.min.css') !!}" rel="stylesheet">
@@ -18,8 +18,24 @@
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-
+		<![endif]-->			
+		<link rel="apple-touch-icon" sizes="57x57" href="{!! asset('assets/icons/apple-icon-57x57.png') !!}">
+		<link rel="apple-touch-icon" sizes="60x60" href="{!! asset('assets/icons/apple-icon-60x60.png') !!}">
+		<link rel="apple-touch-icon" sizes="72x72" href="{!! asset('assets/icons/apple-icon-72x72.png') !!}">
+		<link rel="apple-touch-icon" sizes="76x76" href="{!! asset('assets/icons/apple-icon-76x76.png') !!}">
+		<link rel="apple-touch-icon" sizes="114x114" href="{!! asset('assets/icons/apple-icon-114x114.png') !!}">
+		<link rel="apple-touch-icon" sizes="120x120" href="{!! asset('assets/icons/apple-icon-120x120.png') !!}">
+		<link rel="apple-touch-icon" sizes="144x144" href="{!! asset('assets/icons/apple-icon-144x144.png') !!}">
+		<link rel="apple-touch-icon" sizes="152x152" href="{!! asset('assets/icons/apple-icon-152x152.png') !!}">
+		<link rel="apple-touch-icon" sizes="180x180" href="{!! asset('assets/icons/apple-icon-180x180.png') !!}">
+		<link rel="icon" type="image/png" sizes="192x192"  href="{!! asset('assets/icons/android-icon-192x192.png') !!}">
+		<link rel="icon" type="image/png" sizes="32x32" href="{!! asset('assets/icons/favicon-32x32.png') !!}">
+		<link rel="icon" type="image/png" sizes="96x96" href="{!! asset('assets/icons/favicon-96x96.png') !!}">
+		<link rel="icon" type="image/png" sizes="16x16" href="{!! asset('assets/icons/favicon-16x16.png') !!}">
+		<link rel="manifest" href="{!! asset('assets/icons/manifest.json') !!}">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="{!! asset('assets/icons/ms-icon-144x144.png') !!}">
+		<meta name="theme-color" content="#ffffff">
 		@yield('style')
 	</head>
 	<body>
@@ -41,7 +57,7 @@
 			<div class="col-md-12">
 				<hr>
 				<p class="pull-left">
-					Copyright &COPY; {{ date('Y') }}. All rights reserved.
+					Copyright &COPY; {!! date('Y') !!}. All rights reserved.
 				</p>
 				<p class="pull-right">
 					Thank you for using <a href="https://www.pingpongcms.com">Pingpong CMS</a>
