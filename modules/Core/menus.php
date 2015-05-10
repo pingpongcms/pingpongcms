d81882a013ba673c287ac2bc09c20faec8bf5506
Gravitano <gravitano16@gmail.com>
2015-05-10 21:07:41 +0700

    fix users menu

--- modules/Core/menus.php
+++ modules/Core/menus.php
@@ -6,11 +6,7 @@ Menu::create('sidebar', function ($menu)
 	$menu->setPrefixUrl(cms()->prefix());
 	$menu->header('MAIN NAVIGATION');
 	$menu->url('/', 'Dashboard', ['icon' => 'fa fa-dashboard']);
-	$menu->dropdown('Users', function ($sub)
-	{
-		$sub->url('users/create', 'Add New');
-		$sub->url('users', 'List');
-	}, ['icon' => 'fa fa-users']);
+	$menu->url('/users', 'Users', ['icon' => 'fa fa-users']);
 	$menu->dropdown('Settings', function ($sub)
 	{
 		$sub->url('settings', 'General');
