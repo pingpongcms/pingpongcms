6c080544d49485d50b7edce0d32b2cd4a084174a
Gravitano <gravitano16@gmail.com>
2015-05-10 11:50:28 +0700

    allow to change page title

--- modules/Core/Resources/views/layouts/master.blade.php
+++ modules/Core/Resources/views/layouts/master.blade.php
@@ -39,8 +39,8 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
           <h1>
-            Dashboard
-            <small>Version 2.0</small>
+            {{{ $title or 'Dashboard' }}}
+            <small>{{{ $subtitle or '' }}}</small>
           </h1>
           <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
