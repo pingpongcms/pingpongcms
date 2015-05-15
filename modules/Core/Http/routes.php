2c931e2ef20375333aafe1b8e5f27a969274b8ec
Gravitano <gravitano16@gmail.com>
2015-05-15 16:54:53 +0700

    Added forgot password feature

--- modules/Core/Http/routes.php
+++ modules/Core/Http/routes.php
@@ -6,6 +6,7 @@ $prefix = cms()->prefix();
 
 Route::group(['namespace' => $namespace . '\Auth'], function () {
     Route::controller('auth', 'AuthController');
+    Route::controller('password', 'PasswordController');
 });
 
 Route::group(['prefix' => $prefix, 'middleware' => 'cms.auth', 'namespace' => $namespace], function () {
