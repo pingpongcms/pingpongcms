21e91c84c31b078cb8e8ac1a870f1b1973c1204f
Gravitano <gravitano16@gmail.com>
2015-05-30 13:41:45 +0700

    revert back some routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,31 +11,15 @@
 |
 */
 
-// Route::get('/', 'WelcomeController@index');
+Route::get('/', 'WelcomeController@index');
 
-// Route::controllers([
-//     'auth' => 'Auth\AuthController',
-//     'password' => 'Auth\PasswordController',
-// ]);
+Route::resource('posts', 'PostsController', ['only' => ['index', 'show']]);
+Route::get('posts/{year}/{month}', [
+    'as' => 'posts.archive',
+    'uses' => 'PostsController@archive'
+]);
 
-// Route::resource('posts', 'PostsController', ['only' => ['index', 'show']]);
-// Route::get('posts/{year}/{month}', [
-//     'as' => 'posts.archive',
-//     'uses' => 'PostsController@archive'
-// ]);
-
-// Route::group(['prefix' => 'admin', 'middleware' => 'auth', 'namespace' => 'Admin'], function () {
-//     Route::get('/', [
-//         'as' => 'admin.index',
-//         'uses' => 'DashboardController@index'
-//     ]);
-//     Route::resource('posts', 'PostsController');
-//     Route::resource('categories', 'CategoriesController');
-//     Route::resource('tags', 'TagsController');
-//     Route::resource('users', 'UsersController');
-// });
-
-// Route::get('post/{slug}', [
-//     'as' => 'post',
-//     'uses' => 'PostsController@show'
-// ]);
+Route::get('post/{slug}', [
+    'as' => 'post',
+    'uses' => 'PostsController@show'
+]);
