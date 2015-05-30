b9ef0b817ee00ec184f20985fc721f63c39c4775
Gravitano <gravitano16@gmail.com>
2015-05-30 13:35:56 +0700

    make subtree script more dynamic

--- build/subtree.sh
+++ build/subtree.sh
@@ -1,2 +1,5 @@
-git subtree push --prefix=modules/Core git@github.com:pingpongcms/core.git master --squash
-git subtree push --prefix=modules/Api git@github.com:pingpongcms/api.git master --squash
+export ACTION=$1
+export BRANCH=master
+
+git subtree $ACTION --prefix=modules/Core git@github.com:pingpongcms/core.git $BRANCH --squash
+git subtree $ACTION --prefix=modules/Api git@github.com:pingpongcms/api.git $BRANCH --squash
