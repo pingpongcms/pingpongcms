56c9ea68f5692399c39ed0f403b95f11992f212f
Gravitano <gravitano16@gmail.com>
2015-06-09 20:38:11 +0700

    initial commit

---
+++ Http/Controllers/SettingsController.php
@@ -0,0 +1,12 @@
+<?php namespace Cms\Settings\Http\Controllers;
+
+use Pingpong\Modules\Routing\Controller;
+
+class SettingsController extends Controller {
+	
+	public function index()
+	{
+		return view('settings::index');
+	}
+	
+}
---
+++ Providers/SettingsServiceProvider.php
@@ -0,0 +1,95 @@
+<?php namespace Cms\Settings\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SettingsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Boot the application events.
+	 * 
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->registerConfig();
+		$this->registerTranslations();
+		$this->registerViews();
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{		
+		//
+	}
+
+	/**
+	 * Register config.
+	 * 
+	 * @return void
+	 */
+	protected function registerConfig()
+	{
+		$this->publishes([
+		    __DIR__.'/../Config/config.php' => config_path('settings.php'),
+		]);
+		$this->mergeConfigFrom(
+		    __DIR__.'/../Config/config.php', 'settings'
+		);
+	}
+
+	/**
+	 * Register views.
+	 * 
+	 * @return void
+	 */
+	public function registerViews()
+	{
+		$viewPath = base_path('views/modules/settings');
+
+		$sourcePath = __DIR__.'/../Resources/views';
+
+		$this->publishes([
+			$sourcePath => $viewPath
+		]);
+
+		$this->loadViewsFrom([$viewPath, $sourcePath], 'settings');
+	}
+
+	/**
+	 * Register translations.
+	 * 
+	 * @return void
+	 */
+	public function registerTranslations()
+	{
+		$langPath = base_path('resources/lang/modules/settings');
+
+		if (is_dir($langPath)) {
+			$this->loadTranslationsFrom($langPath, 'settings');
+		} else {
+			$this->loadTranslationsFrom(__DIR__ .'/../Resources/lang', 'settings');
+		}
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
