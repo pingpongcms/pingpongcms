716f479003b417fef8318f23b589f9d54136bf54
Gravitano <gravitano16@gmail.com>
2015-06-11 16:52:45 +0700

    Added post's menus

---
+++ modules/Post/menus.php
@@ -0,0 +1,20 @@
+<?php
+
+$menu = Menu::instance('sidebar');
+
+$menu->dropdown('Posts', function ($sub)
+{
+	$sub->url('administrator/posts', 'All Posts', 1);
+	$sub->url('administrator/posts/create', 'Add New', 2);
+}, 3, ['icon' => 'fa fa-book']);
+
+$menu->dropdown('Pages', function ($sub)
+{
+	$sub->url('administrator/posts?type=page', 'All Pages', 1);
+	$sub->url('administrator/posts/create?type=page', 'Add New', 2);
+}, 4, ['icon' => 'fa fa-flag']);
+
+$menu->whereTitle('Settings', function ($sub)
+{
+	$sub->url('administrator/settings/posts', 'Posts', 2);
+});
