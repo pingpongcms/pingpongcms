36785a8b016abb589204af69065d8ff1d2f2b0f3
Gravitano <gravitano16@gmail.com>
2015-06-11 14:10:12 +0700

    Added create_posts_table migration

---
+++ modules/Post/Database/Migrations/2015_06_11_070848_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('type')->default('post');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->string('image')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+
+}
