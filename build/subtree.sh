a70f92fb35a6ad34bf232d3ecbec08ee2fa5e1ca
Gravitano <gravitano16@gmail.com>
2015-06-11 15:19:35 +0700

    added menus module

--- build/subtree.sh
+++ build/subtree.sh
@@ -4,3 +4,4 @@ export BRANCH=master
 git subtree $ACTION --prefix=modules/Core git@github.com:pingpongcms/core.git $BRANCH --squash
 git subtree $ACTION --prefix=modules/Api git@github.com:pingpongcms/api.git $BRANCH --squash
 git subtree $ACTION --prefix=modules/Settings git@github.com:pingpongcms/settings.git $BRANCH --squash
+git subtree $ACTION --prefix=modules/Menus git@github.com:pingpongcms/menus.git $BRANCH --squash
