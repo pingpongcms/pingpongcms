f95fda654d24bd8a153e0d4c17a222f435905f1b
Gravitano <gravitano16@gmail.com>
2015-06-11 16:56:36 +0700

    add menu icons to submenu items

--- modules/Post/menus.php
+++ modules/Post/menus.php
@@ -4,14 +4,14 @@ $menu = Menu::instance('sidebar');
 
 $menu->dropdown('Posts', function ($sub)
 {
-	$sub->url('administrator/posts', 'All Posts', 1);
-	$sub->url('administrator/posts/create', 'Add New', 2);
+	$sub->url('administrator/posts', 'All Posts', 1, ['icon' => 'fa fa-circle-o']);
+	$sub->url('administrator/posts/create', 'Add New', 2, ['icon' => 'fa fa-circle-o']);
 }, 3, ['icon' => 'fa fa-book']);
 
 $menu->dropdown('Pages', function ($sub)
 {
-	$sub->url('administrator/posts?type=page', 'All Pages', 1);
-	$sub->url('administrator/posts/create?type=page', 'Add New', 2);
+	$sub->url('administrator/posts?type=page', 'All Pages', 1, ['icon' => 'fa fa-circle-o']);
+	$sub->url('administrator/posts/create?type=page', 'Add New', 2, ['icon' => 'fa fa-circle-o']);
 }, 4, ['icon' => 'fa fa-flag']);
 
 $menu->whereTitle('Settings', function ($sub)
