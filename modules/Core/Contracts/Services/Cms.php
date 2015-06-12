28d67c27e8467ed3a27dfee370b1a6cad4c34df1
Gravitano <gravitano16@gmail.com>
2015-06-12 11:25:14 +0700

    allow to change separator when call ->prefix() method, added two methods url & route

--- modules/Core/Contracts/Services/Cms.php
+++ modules/Core/Contracts/Services/Cms.php
@@ -8,4 +8,8 @@ interface Cms
     public function prefix($extra = null);
 
     public function redirect($to, $status = 302, $headers = array(), $secure = null);
+
+    public function route($route, array $parameters = array(), $absolute = true, $route = null);
+    
+    public function url($url, array $parameters = array(), $secure = null);
 }
--- modules/Core/Services/Cms/Cms.php
+++ modules/Core/Services/Cms/Cms.php
@@ -7,13 +7,23 @@ use Pingpong\Cms\Core\Contracts\Services\Cms as CmsContract;
 class Cms implements CmsContract
 {
 
-    public function prefix($extra = null)
+    public function prefix($extra = null, $separator = '/')
     {
-        return config('cms.prefix') . ($extra ? '/' . $extra : null);
+        return config('cms.prefix') . ($extra ? $separator . $extra : null);
     }
 
     public function redirect($to, $status = 302, $headers = array(), $secure = null)
     {
         return app('redirect')->to($this->prefix($to), $status, $headers, $secure);
+    }
+
+    public function route($name, array $parameters = array(), $absolute = true, $route = null)
+    {
+    	return route($this->prefix($name, '.'), $parameters, $absolute, $route);
+    }
+
+    public function url($url, array $parameters = array(), $secure = null)
+    {
+    	return url($this->prefix($url), $parameters, $secure);
     }
 }
