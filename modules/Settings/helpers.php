ce1583c2a0ed050aa03f2826c543f88d96c47e50
Gravitano <gravitano16@gmail.com>
2015-06-12 10:34:02 +0700

    add 'setting' helper

---
+++ modules/Settings/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+use Pingpong\Cms\Settings\Entities\Setting;
+
+if ( ! function_exists('setting'))
+{
+	function setting($key, $default = null)
+	{
+		return Setting::value($key, $default);
+	}
+}
