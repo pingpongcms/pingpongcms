cde45ed228b4842ec658b0d60600d03ebb4e3288
Gravitano <gravitano16@gmail.com>
2015-06-12 14:54:12 +0700

    Add modules menu to Settings menu after settings module booted

--- modules/Core/start.php
+++ modules/Core/start.php
@@ -19,3 +19,15 @@ require __DIR__ . '/helpers.php';
 require __DIR__ . '/menus.php';
 
 require __DIR__ . '/Http/routes.php';
+
+Event::listen('modules.settings.boot', function ()
+{
+	$menu = Menu::instance('sidebar');
+
+	$menu->whereTitle('Settings', function ($sub)
+	{
+		if (!is_null($sub)) {
+			$sub->url('administrator/settings/modules', 'Modules', 3, ['icon' => 'fa fa-circle-o']);
+		}
+	});
+});
