e00b2e5c961a77ea1a6d17106e1a3002e92a3ea4
Gravitano <gravitano16@gmail.com>
2015-06-17 17:26:36 +0700

    update way of modifying menus

--- modules/Post/menus.php
+++ modules/Post/menus.php
@@ -1,17 +1,18 @@
 <?php
 
-$menu = Menu::instance('sidebar');
+Menu::modify('sidebar', function ($menu)
+{
+	$menu->dropdown('Posts', function ($sub) {
+	    $sub->url('administrator/posts', 'All Posts', 1, ['icon' => 'fa fa-circle-o']);
+	    $sub->url('administrator/posts/create', 'Add New', 2, ['icon' => 'fa fa-circle-o']);
+	}, 3, ['icon' => 'fa fa-book']);
 
-$menu->dropdown('Posts', function ($sub) {
-    $sub->url('administrator/posts', 'All Posts', 1, ['icon' => 'fa fa-circle-o']);
-    $sub->url('administrator/posts/create', 'Add New', 2, ['icon' => 'fa fa-circle-o']);
-}, 3, ['icon' => 'fa fa-book']);
+	$menu->dropdown('Pages', function ($sub) {
+	    $sub->url('administrator/posts?type=page', 'All Pages', 1, ['icon' => 'fa fa-circle-o']);
+	    $sub->url('administrator/posts/create?type=page', 'Add New', 2, ['icon' => 'fa fa-circle-o']);
+	}, 4, ['icon' => 'fa fa-flag']);
 
-$menu->dropdown('Pages', function ($sub) {
-    $sub->url('administrator/posts?type=page', 'All Pages', 1, ['icon' => 'fa fa-circle-o']);
-    $sub->url('administrator/posts/create?type=page', 'Add New', 2, ['icon' => 'fa fa-circle-o']);
-}, 4, ['icon' => 'fa fa-flag']);
-
-$menu->whereTitle('Settings', function ($sub) {
-    $sub->url('administrator/settings/posts', 'Posts', 2, ['icon' => 'fa fa-circle-o']);
+	$menu->whereTitle('Settings', function ($sub) {
+	    $sub->url('administrator/settings/posts', 'Posts', 2, ['icon' => 'fa fa-circle-o']);
+	});
 });
