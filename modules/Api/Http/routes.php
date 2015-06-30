bd326882a20f9b5b8861ec2ebe44362bd801b4f1
Gravitano <gravitano16@gmail.com>
2015-06-30 13:01:26 +0700

    PSR-2

--- modules/Api/Http/routes.php
+++ modules/Api/Http/routes.php
@@ -2,13 +2,13 @@
 
 Route::group(['prefix' => 'api', 'namespace' => 'Pingpong\Cms\Api\Http\Controllers'], function () {
     Route::resource('users', 'UsersController', [
-    	'names' => [
-    		'index' => 'api.users.index',
-    		'store' => 'api.users.store',
-    		'update' => 'api.users.update',
-    		'destroy' => 'api.users.destroy',
-    		'show' => 'api.users.show',
-    	],
-    	'only' => ['index', 'store', 'update', 'show', 'destroy']
+        'names' => [
+            'index' => 'api.users.index',
+            'store' => 'api.users.store',
+            'update' => 'api.users.update',
+            'destroy' => 'api.users.destroy',
+            'show' => 'api.users.show',
+        ],
+        'only' => ['index', 'store', 'update', 'show', 'destroy']
     ]);
 });
