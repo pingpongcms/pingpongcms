bc1c6749866bf4dbb61cc445d3855fcf240b83fd
Gravitano <gravitano16@gmail.com>
2015-06-30 14:51:41 +0700

    auto prepend suffix

--- modules/Core/Services/Cms/Cms.php
+++ modules/Core/Services/Cms/Cms.php
@@ -19,7 +19,7 @@ class Cms implements CmsContract
 
     public function route($name, $parameters = array(), $absolute = true, $route = null)
     {
-    	return route($this->prefix($name, '.'), $parameters, $absolute, $route);
+    	return route('admin.' . $name, $parameters, $absolute, $route);
     }
 
     public function url($url, array $parameters = array(), $secure = null)
