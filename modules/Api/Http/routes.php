710fe0d68ef7ee080ceb790f7e6c2c7fb43382e7
Gravitano <gravitano16@gmail.com>
2015-06-30 12:59:23 +0700

    Add route names

--- modules/Api/Http/routes.php
+++ modules/Api/Http/routes.php
@@ -1,5 +1,12 @@
 <?php
 
 Route::group(['prefix' => 'api', 'namespace' => 'Pingpong\Cms\Api\Http\Controllers'], function () {
-    Route::resource('users', 'UsersController');
+    Route::resource('users', 'UsersController', [
+    	'names' => [
+    		'index' => 'api.users.index',
+    		'store' => 'api.users.store',
+    		'update' => 'api.users.update',
+    		'destroy' => 'api.users.destroy',
+    	]
+    ]);
 });
