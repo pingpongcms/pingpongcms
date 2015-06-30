e4f5174d7a1d59e097754ae9b231c0a9cef6aee6
Gravitano <gravitano16@gmail.com>
2015-06-30 12:56:09 +0700

    PSR-2

--- modules/Core/Http/routes.php
+++ modules/Core/Http/routes.php
@@ -4,22 +4,22 @@ $namespace = 'Pingpong\Cms\Core\Http\Controllers';
 
 Route::group(['namespace' => $namespace . '\Auth'], function () {
     Route::controller('auth', 'AuthController', [
-    	'getLogin' => 'login.index',
-    	'postLogin' => 'login.store',
-    	'getRegister' => 'register.index',
-    	'postRegister' => 'register.store',
-    	'getLogout' => 'logout',
-    	'getConfirmation' => 'user.confirmation'
+        'getLogin' => 'login.index',
+        'postLogin' => 'login.store',
+        'getRegister' => 'register.index',
+        'postRegister' => 'register.store',
+        'getLogout' => 'logout',
+        'getConfirmation' => 'user.confirmation'
     ]);
     Route::controller('password', 'PasswordController', [
-    	'getEmail' => 'password.email.index',
-    	'postEmail' => 'password.email.store',
-    	'getReset' => 'password.reset.index',
-    	'postReset' => 'password.reset.store'
+        'getEmail' => 'password.email.index',
+        'postEmail' => 'password.email.store',
+        'getReset' => 'password.reset.index',
+        'postReset' => 'password.reset.store'
     ]);
 });
 
-Route::group(['prefix' => cms()->prefix(), 'middleware' => config('cms.middleware'),'namespace' => $namespace], function () {
+Route::group(['prefix' => cms()->prefix(), 'middleware' => config('cms.middleware'), 'namespace' => $namespace], function () {
     Route::get('/', 'CoreController@index');
     Route::get('/users', 'UsersController@index');
 });
