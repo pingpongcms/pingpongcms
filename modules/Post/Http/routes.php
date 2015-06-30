a05317ebfb1bab26f3c715e80877fceed68a134f
Gravitano <gravitano16@gmail.com>
2015-06-30 14:47:22 +0700

    Add route names

--- modules/Post/Http/routes.php
+++ modules/Post/Http/routes.php
@@ -6,6 +6,16 @@ Route::group(['prefix' => 'post', 'namespace' => 'Pingpong\\Cms\Post\Http\Contro
 
 Event::listen('backend.routes', function ($router) {
     $router->group(['namespace' => 'Pingpong\Cms\Post\Http\Controllers\Admin'], function () {
-        Route::resource('posts', 'PostsController');
+        Route::resource('posts', 'PostsController', [
+        	'names' => [
+        		'index' => 'admin.posts.index',
+        		'create' => 'admin.posts.create',
+        		'store' => 'admin.posts.store',
+        		'edit' => 'admin.posts.edit',
+        		'update' => 'admin.posts.update',
+        		'destroy' => 'admin.posts.destroy',
+        		'show' => 'admin.posts.show',
+        	]
+        ]);
     });
 });
